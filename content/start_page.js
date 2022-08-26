e09c4224b94716db08c1c9cc3e62194f00a42393
mgelios <antonnadezhkin@gmail.com>
2022-08-26 13:26:34 +0200

    separated fast startpage from all bookmarks

---
+++ content/start_page.js
@@ -0,0 +1,117 @@
+const speed_dial_links = 
+[{
+    title: "Bookmarks",
+    link: "./bookmarks.html",
+    linkButtonText: "Go bookmarks",
+    color: "secondary",
+    badgeColor: "secondary",
+    badgeText: "B",
+    badgeTextColor: "text-white"
+}, {
+    title: "Notion",
+    link: "./pages/tools.html",
+    linkButtonText: "Go Notion",
+    linkButtonTextColor: "text-dark",
+    color: "light",
+    badgeColor: "light",
+    badgeText: "[N]",
+    badgeTextColor: "text-dark"
+}, {
+    title: "Youtube",
+    link: "https://www.youtube.com",
+    linkButtonText: "Go youtube",
+    color: "danger",
+    badgeColor: "danger",
+    badgeText: "You",
+    badgeTextColor: "text-white"
+}, {
+    title: "GMail",
+    link: "https://mail.google.com/mail/u/0/",
+    linkButtonText: "Go GMail",
+    linkButtonTextColor: "text-dark",
+    color: "warning",
+    badgeColor: "warning",
+    badgeText: "G",
+    badgeTextColor: "text-dark"
+}, {
+    title: "Calendar",
+    link: "https://calendar.google.com/calendar/u/0/r",
+    linkButtonText: "Go YouTube",
+    linkButtonTextColor: "text-dark",
+    color: "light",
+    badgeColor: "light",
+    badgeText: "C",
+    badgeTextColor: "text-dark"
+}, {
+    title: "GitHub",
+    link: "https://github.com/",
+    linkButtonText: "Go github",
+    color: "secondary",
+    badgeColor: "secondary",
+    badgeText: "Git",
+    badgeTextColor: "text-white"
+}, {
+    title: "Trello",
+    link: "https://trello.com/",
+    linkButtonText: "Go trello",
+    color: "primary",
+    badgeColor: "primary",
+    badgeText: "T",
+    badgeTextColor: "text-white"
+}, {
+    title: "Zerkalo.io",
+    link: "https://www.zerkalo.io",
+    linkButtonText: "Go Zerkalo",
+    color: "danger",
+    badgeColor: "danger",
+    badgeText: "Z",
+    badgeTextColor: "text-white"
+}, {
+    title: "Dev.by",
+    link: "https://www.devby.io",
+    linkButtonText: "Go dev.by",
+    color: "primary",
+    badgeColor: "primary",
+    badgeText: "D",
+    badgeTextColor: "text-white"
+}, {
+    title: "Linux.org.ru",
+    link: "https://www.linux.org.ru",
+    linkButtonText: "Go LOR",
+    color: "secondary",
+    badgeColor: "secondary",
+    badgeText: "L",
+    badgeTextColor: "text-white"
+}, {
+    title: "Gwer's K",
+    link: "https://gwer.github.io/kitchen/",
+    linkButtonText: "Go Gwer's K",
+    color: "success",
+    badgeColor: "success",
+    badgeText: "GK",
+    badgeTextColor: "text-white"
+}, {
+    title: "Razrabs",
+    link: "https://razrabs.ru/",
+    linkButtonText: "Go Razrabs",
+    color: "secondary",
+    badgeColor: "secondary",
+    badgeText: "R",
+    badgeTextColor: "text-white"
+}, {
+    title: "Onliner",
+    link: "https://www.onliner.by",
+    linkButtonText: "Go Onliner",
+    color: "danger",
+    badgeColor: "danger",
+    badgeText: "O",
+    badgeTextColor: "text-white"
+}, {
+    title: "Render.ru",
+    link: "https://www.render.ru",
+    linkButtonText: "Go Render.ru",
+    color: "info",
+    badgeColor: "info",
+    badgeText: "R",
+    badgeTextColor: "text-white"
+}, ];
