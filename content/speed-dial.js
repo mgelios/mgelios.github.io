dc0d3b01692deafab01486ae1d5655df0788b50f
mgelios <antonnadezhkin@gmail.com>
2024-10-27 16:52:23 +0100

    updated links

--- content/speed-dial.js
+++ content/speed-dial.js
@@ -2,116 +2,91 @@ const speed_dial_links =
 [{
     title: "Notion",
     link: "https://notion.so/",
-    linkButtonText: "Go Notion",
-    linkButtonTextColor: "text-dark",
-    color: "light",
     badgeColor: "light-4",
     badgeText: "[N]",
     badgeTextColor: "dark-1"
 }, {
     title: "GMail",
     link: "https://mail.google.com/mail/u/0/",
-    linkButtonText: "Go GMail",
-    linkButtonTextColor: "text-dark",
-    color: "warning",
     badgeColor: "orange-4",
     badgeText: "G",
     badgeTextColor: "dark-3"
 }, {
     title: "Calendar",
     link: "https://calendar.google.com/calendar/u/0/r",
-    linkButtonText: "Go YouTube",
-    linkButtonTextColor: "text-dark",
-    color: "light",
     badgeColor: "blue-3",
     badgeText: "C",
     badgeTextColor: "light-3"
 }, {
     title: "GitHub",
     link: "https://github.com/",
-    linkButtonText: "Go github",
-    color: "secondary",
     badgeColor: "dark-2",
     badgeText: "Git",
     badgeTextColor: "light-3"
 }, {
     title: "Youtube",
     link: "https://www.youtube.com",
-    linkButtonText: "Go youtube",
-    color: "danger",
     badgeColor: "red-3",
     badgeText: "You",
     badgeTextColor: "light-3"
 }, {
     title: "Zerkalo.io",
     link: "https://www.zerkalo.io",
-    linkButtonText: "Go Zerkalo",
-    color: "danger",
     badgeColor: "red-3",
     badgeText: "Z",
     badgeTextColor: "light-3"
 }, {
     title: "Linux.org.ru",
     link: "https://www.linux.org.ru",
-    linkButtonText: "Go LOR",
-    color: "secondary",
     badgeColor: "dark-2",
     badgeText: "L",
     badgeTextColor: "light-3"
 }, {
     title: "Vas3k club",
     link: "https://vas3k.club/",
-    linkButtonText: "Go Vas3k",
-    color: "secondary",
     badgeColor: "dark-2",
     badgeText: "V",
     badgeTextColor: "light-3"
 }, {
     title: "3D news",
     link: "https://3dnews.ru/",
-    linkButtonText: "Go 3D news",
-    color: "secondary",
     badgeColor: "blue-2",
     badgeText: "3D",
     badgeTextColor: "light-3"
 }, {
     title: "Render",
     link: "https://render.ru",
-    linkButtonText: "Go Render",
-    color: "secondary",
     badgeColor: "green-2",
     badgeText: "R",
     badgeTextColor: "light-3"
 }, {
     title: "The Warsaw",
     link: "https://www.the-warsaw.com/",
-    linkButtonText: "Go LOR",
-    color: "secondary",
     badgeColor: "blue-4",
     badgeText: "T",
     badgeTextColor: "light-3"
 }, {
     title: "ChatGPT",
     link: "https://chat.openai.com",
-    linkButtonText: "Go ChatGPT",
-    color: "secondary",
     badgeColor: "green-2",
     badgeText: "C",
     badgeTextColor: "light-3"
 }, {
     title: "Joyreactor",
     link: "https://www.joyreactor.cc",
-    linkButtonText: "Go joyreactor",
-    color: "secondary",
     badgeColor: "orange-2",
     badgeText: "J",
     badgeTextColor: "light-3"
 }, {
     title: "LinkedIn",
     link: "https://linkedin.com/",
-    linkButtonText: "Go linkedin",
-    color: "secondary",
     badgeColor: "blue-2",
     badgeText: "L",
     badgeTextColor: "light-3"
+}, {
+    title: "Sakhtv",
+    link: "https://sakhtv.ru/",
+    badgeColor: "dark-1",
+    badgeText: "S",
+    badgeTextColor: "light-2"
 }];
